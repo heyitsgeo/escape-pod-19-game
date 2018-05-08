be302828e37edd1c170f25d7a5b3269da5ea7594
Jason Kell <jasonkell86@gmail.com>
2018-05-07 22:14:38 -0400

    Reorg of the code.

---
+++ Assets/Scripts/Controllers/MainMenuController.cs
@@ -0,0 +1,47 @@
+﻿namespace Assets.Scripts.Controllers
+{
+    using System;
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
+    using UnityEngine.UI;
+
+    public class MainMenuController : MonoBehaviour
+    {
+        [SerializeField]
+        private Button _startButton, _quitButton;
+
+        [SerializeField]
+        private string _levelScene;
+
+        protected void Start()
+        {
+            Button start = _startButton.GetComponent<Button>();
+            Button quit = _quitButton.GetComponent<Button>();
+
+            // Call start button handler on click
+            start.onClick.AddListener(LoadLevelsScene);
+
+            // Call quit button handle on click
+            quit.onClick.AddListener(QuitGame);
+        }
+
+        protected void LoadLevelsScene()
+        {
+            try
+            {
+                Debug.Log("Load Levels");
+                SceneManager.LoadScene(_levelScene);
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e);
+            }
+        }
+
+        protected void QuitGame()
+        {
+            Debug.Log("Quit Game");
+            Application.Quit();
+        }
+    }
+}
--- Assets/Scripts/Models/LevelModel.cs
+++ Assets/Scripts/Models/LevelModel.cs
@@ -1,11 +1,32 @@
 ﻿namespace Assets.Scripts.Models
 {
-  using System;
-  [Serializable]
-  public class LevelModel
-  {
-    public string SceneName;
-    public string LevelName;
-    public bool Locked;
-  }
-}
+    using System;
+    using UnityEngine;
+
+    [Serializable]
+    public class LevelModel
+    {
+        [SerializeField]
+        private string _sceneName;
+        public string SceneName
+        {
+            get { return _sceneName; }
+            set { _sceneName = value; }
+        }
+        [SerializeField]
+        private string _levelName;
+        public string LevelName
+        {
+            get { return _levelName; }
+            set { _levelName = value; }
+        }
+
+        [SerializeField]
+        private bool _locked;
+        public bool Locked
+        {
+            get { return _locked; }
+            set { _locked = value; }
+        }
+    }
+}   
