d0204ee1d4cf5c4a34d43f33e366535a83076823
Jason Kell <jasonkell86@gmail.com>
2018-05-08 22:03:50 -0400

    First commit

---
+++ Assets/Scripts/Controllers/InventoryController.cs
@@ -0,0 +1,28 @@
+﻿namespace Assets.Scripts.Controllers
+{
+    using Assets.Scripts.Models;
+    using UnityEngine;
+    using UnityEngine.UI;
+    
+    public class InventoryController : MonoBehaviour
+    {
+        public Image[] ItemImages = new Image[NumItemSlots];
+        public ItemModel[] Items = new ItemModel[NumItemSlots];
+
+        public const int NumItemSlots = 4;
+
+        public void AddItem(ItemModel itemToAdd)
+        {
+            for (int i = 0; i < Items.Length; i++)
+            {
+                var item = Items[i];
+
+                if (item == null)
+                {
+                    item = itemToAdd;
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Models/ItemModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Assets.Scripts.Models
+{
+    using UnityEngine;
+
+    [CreateAssetMenu]
+    public class ItemModel
+    {
+        [SerializeField]
+        private Sprite _itemSprite;
+        public Sprite ItemSprite
+        {
+            get { return _itemSprite; }
+            set { _itemSprite = value; }
+        }
+    }
+}
